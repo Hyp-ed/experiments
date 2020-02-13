bd29a1d3f7f10c372b472cfd36d74728190bcd82
Branislav Pilnan <B.Pilnan@gmail.com>
2020-02-13 18:08:30 +0000

    Add manual control

--- PULSES/src/main.cpp
+++ PULSES/src/main.cpp
@@ -1,23 +1,20 @@
 #include <Arduino.h>
 #define FREQUENCY 1000
 #define TIME 3
+#define PIN 3
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(3 , OUTPUT);
-
-  Serial.begin(9600);
-
+void send_pulses() {
   int x = 1000000 / FREQUENCY / 2;
   int pulse_count = TIME * FREQUENCY;
   int timer = millis();
 
   for (int i = 0; i < pulse_count; i++) {
-    digitalWrite(3, HIGH);
+    digitalWrite(PIN, LOW);
     delayMicroseconds(x);
-    digitalWrite(3, LOW);
+    digitalWrite(PIN, HIGH);
     delayMicroseconds(x);
-    }
+  }
+  digitalWrite(PIN, LOW);
 
   timer = millis() - timer;
   Serial.print("Time [s]: ");
@@ -28,6 +25,19 @@ void setup() {
   Serial.println((pulse_count / (timer / 1000)));
 }
 
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN , OUTPUT);
+  digitalWrite(PIN, LOW);
+  Serial.begin(9600);
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
+  while (Serial.available() == 0) {/* Spin. */}
+  String s = Serial.readString();
+  Serial.println(s);
+  if (s.equals("GO"))
+    send_pulses();
 }
